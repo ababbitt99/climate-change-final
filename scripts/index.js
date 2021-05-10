9722158b29c91fdee9cd9457e414c29d99ee5d9d
Zubair <zubairaziz.dev@gmail.com>
2021-05-09 20:22:04 -0400

    Disable unused map controls

--- scripts/index.js
+++ scripts/index.js
@@ -4,5 +4,8 @@ function initMap() {
   map = new google.maps.Map(document.getElementById("map"), {
     center: { lat: 43.052425, lng: -75.405792 },
     zoom: 10,
+    mapTypeControl: false,
+    streetViewControl: false,
+    rotateControlOptions: false,
   });
 }
